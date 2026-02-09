d19ef856325ff43c6e0047f74bee23a758ddeedf
Viraj Chandra <virajchandra51@gmail.com>
2026-02-09 17:22:44 +0530

    Initial logo site

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./assets/logo.png";
+
+function App() {
+    return (
+        <div className="container">
+            <img src={logo} alt="Logo" className="logo" />
+        </div>
+    );
+}
+
+export default App;
